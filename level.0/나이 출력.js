d2bd01a64d1203c89c681dc189a0791c9ca33453
HYHYJ <jjangdaram@naver.com>
2023-07-09 22:43:15 +0900

    first commit

---
+++ level.0/나이 출력.js
@@ -0,0 +1,6 @@
+function solution(age) {
+  var answer = 0;
+  answer = 2022 + 1 - age;
+  return answer;
+}
+solution(40);
---
+++ level.0/숫자 비교하기.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+  var answer = 0;
+  if (num1 === num2) {
+    answer = 1;
+  } else {
+    answer = -1;
+  }
+
+  return answer;
+}
+
+solution(2, 3);
