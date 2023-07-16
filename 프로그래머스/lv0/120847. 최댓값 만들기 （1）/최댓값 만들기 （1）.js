28256f51b92d7ecc5c7c323b65c34043c16dd11b
HYHYJ <101866872+HYHYJ@users.noreply.github.com>
2023-07-16 18:38:43 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,6 +1,5 @@
 function solution(numbers) {
     var answer = 0;
     numbers.sort((a, b) => b - a)
-    answer = numbers[0] * numbers[1] ;
-    return answer;
+    return answer = numbers[0] * numbers[1] ;
 }
