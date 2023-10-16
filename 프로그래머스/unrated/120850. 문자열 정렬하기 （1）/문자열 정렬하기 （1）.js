265acc397d142f22f65231230e47917091e75e1a
장효윤 <101866872+HYHYJ@users.noreply.github.com>
2023-10-16 23:13:53 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,6 +1,5 @@
 function solution(str) {
-    
-    
-    let answer = str.split("").map((i)=> parseInt(i)).filter((i) => Number(i) || i === 0 ).sort()
+    let answer = str.split("").map((i)=> parseInt(i))
+    .filter((i) => Number(i) || i === 0 ).sort()
     return answer;
 }
