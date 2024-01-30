4acd45890e8790f6abf347fada263c6510db78c3
장효윤 <101866872+HYHYJ@users.noreply.github.com>
2024-01-30 15:01:00 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,16 +1,3 @@
-// function solution(arr) {
-//     let answer = 0
-//      arr.map((i, idx)=>{
-//         i.map((j, jidx)=>{
-//           return (arr[idx][jidx] === arr[jidx][idx])
-//         }
-//         return i === false ? answer = 1 : answer
-//     )
-// }
-//    )
-// return answer
-// }\
-
 function solution(arr) {
     let answerFalse = 0;
     arr.map((i, idx) => {
