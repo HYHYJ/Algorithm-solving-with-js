0164cfbd620f904856fa39b81ae9ab4808d001d3
장효윤 <101866872+HYHYJ@users.noreply.github.com>
2024-02-12 23:43:50 +0900

    [level 0] Title: k의 개수, Time: 3.67 ms, Memory: 35.1 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,8 +1,12 @@
+// function solution(i, j, k) {
+//     let a ='';
+//     for(i; i<=j; i++){a += i}
+// 	return a.split(k).length-1
+// }
 function solution(i, j, k) {
-    let a ='';
-    for(i; i<=j; i++){a += i}
-	return a.split(k).length-1
+    return Array(j-i+1).fill(i).map((v,i)=>v+i).join('').split(k).length-1;
 }
+
 
 // function solution(i, j, k) {
 //     let result = 0;
